c1056eed140e6eb0385a1a15428e7059f0c4cc1f
Jiandong Ma <jiandong.ma.cn@gmail.com>
2026-01-07 15:40:37 +0800

    Add job endpoint integration test
    Signed-off-by: Jiandong Ma <jiandong.ma.cn@gmail.com>

--- src/DotNetUnknown/Scheduling/Job1.cs
+++ src/DotNetUnknown/Scheduling/Job1.cs
@@ -6,7 +6,7 @@ public class Job1(ILogger<Job1> logger) : IJob
 {
     public async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        await Task.Delay(2000, stoppingToken);
+        await Task.Delay(1000, stoppingToken);
         logger.LogInformation("Job 1 completed");
     }
 }
--- src/DotNetUnknown/Scheduling/Job2.cs
+++ src/DotNetUnknown/Scheduling/Job2.cs
@@ -6,7 +6,7 @@ public class Job2(ILogger<Job2> logger) : IJob
 {
     public async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        await Task.Delay(5000, stoppingToken);
+        await Task.Delay(3000, stoppingToken);
         logger.LogInformation("Job 2 completed");
     }
 }
